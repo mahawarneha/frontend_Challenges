d1bf1b795b862c7b325b5f1b78e81b348e4e791a
Neha <109570765+mahawarneha@users.noreply.github.com>
2024-06-21 16:19:59 +0530

    Update script.js

--- phoneNum masSecu/script.js
+++ phoneNum masSecu/script.js
@@ -1,1 +1,14 @@
+///for example : 98******09
 
+
+function massPN(PN){
+    let fTwoDig=PN.substring(0,2);
+    let lTwoDig=PN.substring(8);
+    let maskedDig=PN.substring(2,8).replace(/\d/g,"*");
+    
+    let maskedPhonrNumber=fTwoDig +maskedDig +lTwoDig;
+    return maskedPhonrNumber;
+    
+}
+let number='0987654321'
+console.log(massPN(number));
